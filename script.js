5d0817209cb7e5e8ebec0ee14b7d92f50d7f9d0b
Sothyra Chan <schan256@my.centennialcollege.ca>
2025-01-01 21:34:01 -0500

    refined the regex search and created nested functions in evalFormula()

--- script.js
+++ script.js
@@ -32,7 +32,14 @@ const charRange = (start, end) =>
 
 const evalFormula = id => {
   const idToText = id => cells.find(cell => cell.id === id).value;
-  const rangeRegex = /([A-J])/;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => {
+    const inner = character => {
+      return idToText(character + num);
+    }
+    return inner;
+  }
 };
 
 window.onload = () => {
